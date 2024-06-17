8f7baecd3d4655a91f5dfa2b09a54157e60f036f
cafontpal <cfontpal@gmail.com>
2024-06-17 02:54:47 -0400

    Setting up DataAccess

---
+++ AdvancedProgrammingNew.DataAccess/Contexts/ApplicationContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdvancedProgrammingNew.Domain.Entities.Equipments;
+using AdvancedProgrammingNew.Domain.Entities.Planification;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvancedProgrammingNew.DataAccess.Contexts
+{
+    public class ApplicationContext : DbContext
+    {
+        #region Tables
+
+        public DbSet<Equipment> Equipments { get; set; }
+
+        public DbSet<Maintenance> Maintenances { get; set; }
+
+        public DbSet<Calibration> Calibrations { get; set; }
+
+        #endregion
+
+        public ApplicationContext() { }
+
+        public ApplicationContext(string connectionString) : base(GetOptions(connectionString)) { }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private static DbContextOptions GetOptions(string connectionString)
+        {
+            return SqliteDbContextOptionsBuilderExtensions.UseSqlite(new DbContextOptionsBuilder(), connectionString).Options;
+        }
+    }
+}
---
+++ AdvancedProgrammingNew.DataAccess/FluentConfigurations/Common/EntityTypeConfigurationBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdvancedProgrammingNew.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AdvancedProgrammingNew.DataAccess.FluentConfigurations.Common
+{
+    public abstract class EntityTypeConfigurationBase<T> : IEntityTypeConfiguration<T> where T : Entity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id).IsRequired();
+        }
+
+        
+
+    }
+}
