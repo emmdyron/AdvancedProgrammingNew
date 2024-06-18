e10b87fe73aab62728054ea13cf1ff427e1a599f
Dayron Rodriguez Diaz <dayronrdiaz@gmail.com>
2024-06-18 08:00:59 +0200

    Agregando la configuracion de la Planificacion

---
+++ AdvancedProgrammingNew.DataAccess/FluentConfigurations/Planifications/PlanificationTypeConfiguationBase.cs
@@ -0,0 +1,24 @@
+﻿using AdvancedProgrammingNew.DataAccess.FluentConfigurations.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdvancedProgrammingNew.Domain.Entities.Planification;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace AdvancedProgrammingNew.DataAccess.FluentConfigurations.Planifications
+{
+    public class PlanificationEntityTypeConfiguationBase : EntityTypeConfigurationBase<Planification>
+    {
+        public override void Configure(EntityTypeBuilder<Planification> builder)
+        {
+            builder.ToTable("Planifications");
+            base.Configure(builder);
+        }
+
+
+    }
+}
